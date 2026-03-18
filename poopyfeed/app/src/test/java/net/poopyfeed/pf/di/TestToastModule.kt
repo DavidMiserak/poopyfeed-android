7cacfb4819911de78aa8c2dc6f6706f00d9a8b5e
David Miserak <David.Miserak@gmail.com>
2026-03-17 21:56:16 -0400

    test(android): add Hilt test module for ToastManager injection
    - Replaces ToastModule in test environments
    - Binds TestToastManager for unit tests
    - Automatic injection via @HiltViewModel and viewModels()

---
+++ poopyfeed/app/src/test/java/net/poopyfeed/pf/di/TestToastModule.kt
@@ -0,0 +1,21 @@
+package net.poopyfeed.pf.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import net.poopyfeed.pf.ui.toast.TestToastManager
+import net.poopyfeed.pf.ui.toast.ToastManager
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [ToastModule::class]
+)
+abstract class TestToastModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindTestToastManager(impl: TestToastManager): ToastManager
+}
